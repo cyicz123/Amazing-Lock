00db826034a08b1d4e6c496b270cf039b1b13f8b
cyicz123 <602921001@qq.com>
2021-01-14 10:14:16 +0800

    日志功能需改进

--- main.py
+++ main.py
@@ -19,8 +19,10 @@ liveness_control = 'NORMAL'  # 活体检测级别
 status = 0
 face = Queue(3)
 users = Queue(5)
-frames= Queue(10)
+frames = Queue(10)
 net = threading.Event()
+flag = threading.Event()
+lock = threading.Lock()
 logging.basicConfig(filename='log.txt', level=logging.INFO, format='%(asctime)s %(levelname)s %(thread)s %(message)s')
 
 capture = cv2.VideoCapture(0)
@@ -29,17 +31,18 @@ capture.set(cv2.CAP_PROP_FRAME_HEIGHT, 480)
 
 face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
 
+
 class GetAccessToken(threading.Thread):
     def __init__(self, threadName='getAccessToken'):
         super(GetAccessToken, self).__init__(name=threadName)
         self.host = 'https://aip.baidubce.com/oauth/2.0/token?grant_type=client_credentials&client_id={}&client_secret={}'.format(
             client_id, client_secret)
-        self.startTime, self.expires_in = 0, 0
+        self.startTime, self.end, self.expires_in = 0, 0, 0
 
     def run(self):
         while True:
             self.end = time.time()
-            if self.end - self.startTime > self.expires_in or not net.isSet():
+            if self.end - self.startTime > self.expires_in or not net.isSet():  # 超时或者没网就重连
                 self.update_token()
                 time.sleep(2)
             else:
@@ -64,7 +67,7 @@ class GetAccessToken(threading.Thread):
             else:
                 net.clear()
                 logging.warning('there is no response!')
-        except:
+        except ConnectionError:
             net.clear()
             logging.warning('No Internet Connection！')
 
@@ -81,7 +84,7 @@ class SerchFace(threading.Thread):
         }
         self.params = ''
         self.headers = {'content-type': 'application/json'}
-        self.base64Str=''
+        self.base64Str = ''
 
     def img2base64(self):
         img = face.get()
@@ -90,45 +93,49 @@ class SerchFace(threading.Thread):
 
     def run(self):
         while True:
-            if net.isSet():
-                self.requestUrl = "https://aip.baidubce.com/rest/2.0/face/v3/search?access_token=" + access_token
-                self.img2base64()
-                self.paramsDict['image'] = self.base64Str
-                self.params = json.dumps(self.paramsDict)
+
+            self.requestUrl = "https://aip.baidubce.com/rest/2.0/face/v3/search?access_token=" + access_token
+            self.img2base64()
+            self.paramsDict['image'] = self.base64Str
+            self.params = json.dumps(self.paramsDict)
+            try:
                 response = requests.post(self.requestUrl, data=self.params, headers=self.headers)
                 if response:
                     data = response.json()
                     if data['error_code']:
                         logging.warning('error_code:{},error_msg:{}'.format(data['error_code'], data['error_msg']))
+                        global status
+                        with lock:
+                            status = 2  # 验证失败，重置status为2，再发请求
                     else:
                         result = data['result']['user_list']
                         for user in result:
-                            if user['score']<90:
-                                logging.info('group_id:{}-user_id:{}-score:{}%'.format(user['group_id'],user['user_id'],user['score']))
-                            else:
-                                users.put({'group_id':user['group_id'],'user_id':user['user_id']})
+                            users.put(user)
                 else:
                     logging.warning('there is no response!')
-            else:
+            except ConnectionError:
+                net.clear()
+                logging.warning('No Internet Connection！')
                 net.wait()
+
 
 class Pass(threading.Thread):
     def __init__(self, threadName='Pass'):
         super(Pass, self).__init__(name=threadName)
-        self.user={}
+        self.user = {}
 
     def run(self) -> None:
         while True:
-            self.user=users.get()
-            logging.info('group_id:{}-user_id:{}'.format(self.user['group_id'],self.user['user_id']))
-            global status
-            status=-1
-
-class Draw(threading.Thread):
-    def __init__(self,threadName='Draw'):
-        super(Draw, self).__init__(name=threadName)
-
-    def run(self) -> None:
+            self.user = users.get()
+            if self.user['score'] > 90:
+                logging.info('group_id:{}-user_id:{}'.format(self.user['group_id'], self.user['user_id']))
+                flag.set()
+                time.sleep(2)
+                flag.clear()
+            else:
+                global status
+                with lock:
+                    status = 2  # 验证失败，重置status为2，再发请求
 
 
 if __name__ == '__main__':
@@ -139,16 +146,13 @@ if __name__ == '__main__':
     consumer = SerchFace()
     consumer.setDaemon(True)
     consumer.start()
-    p=Pass()
+    p = Pass()
     p.setDaemon(True)
     p.start()
 
     while True:
-        frameList=[]
         ret, img = capture.read()
-        frameList.append(img)
-        image = img.copy()##
-        frameList.append({'rectangle':[(106, 80), (533, 400), (255, 255, 255), 5]})
+        image = img.copy()
         cv2.rectangle(img, (106, 80), (533, 400), (255, 255, 255), 5)
         cv2.putText(img, 'Please put your face in this frame.', (140, 70),
                     cv2.FONT_HERSHEY_SCRIPT_COMPLEX,
@@ -159,36 +163,59 @@ if __name__ == '__main__':
                                               minNeighbors=5,
                                               minSize=(30, 30),
                                               flags=cv2.CASCADE_SCALE_IMAGE)
-        flag = False
+        '''
+        flag.isSet()表示是否放行
+        status表示检测的连续人脸的序号
+        若status>0 flag为false说明检测到人脸，正在交给百度识别
+        若status>0 flag为true说明认证成功，放行。且此时不再给百度发请求，status继续增加
+        若status在[3,5]间，则发请求
+        '''
+        if not len(faces):
+            with lock:
+                status = 0
         for (x, y, w, h) in faces:
-            flag=True
             cv2.rectangle(img, (x, y), (x + w, y + h), (255, 0, 0), 1)
             if x < 106 and x + w > 533 and y < 80 and y + h > 400 and w * h < 900:
-                status=0
+                with lock:
+                    status = 0
                 continue
-            if not status:
-                status += 1
+            else:
+                with lock:
+                    status += 1
+            if status == 1:
                 cv2.putText(img, 'detect a face', (10, 30),
                             cv2.FONT_HERSHEY_SCRIPT_COMPLEX,
                             0.8, (255, 255, 255), 2)
-                break
-            elif status == 1:
-                status += 1
+
+            elif status == 2:
                 cv2.putText(img, 'detect a face', (10, 30),
                             cv2.FONT_HERSHEY_SCRIPT_COMPLEX,
                             0.8, (255, 255, 255), 2)
-                break
-            elif status >1 and status <5:
-                face.put(image)
-                status+=1
-            if status==-1:
-                cv2.putText(img, 'Verify successfully!', (10, 30),
-                            cv2.FONT_HERSHEY_SCRIPT_COMPLEX,
-                            0.8, (255, 255, 255), 1)
 
+            elif 2 < status < 6:
+                if not flag.isSet():
+                    face.put(image)
+                    cv2.putText(img, 'detecting', (10, 30),
+                                cv2.FONT_HERSHEY_SCRIPT_COMPLEX,
+                                0.8, (255, 255, 255), 2)
 
+                else:
+                    cv2.putText(img, 'Verify successfully!', (10, 30),
+                                cv2.FONT_HERSHEY_SCRIPT_COMPLEX,
+                                0.8, (255, 255, 255), 2)
+
+            else:
+                if not flag.isSet():
+                    cv2.putText(img, 'detecting', (10, 30),
+                                cv2.FONT_HERSHEY_SCRIPT_COMPLEX,
+                                0.8, (255, 255, 255), 2)
+
+                else:
+                    cv2.putText(img, 'Verify successfully!', (10, 30),
+                                cv2.FONT_HERSHEY_SCRIPT_COMPLEX,
+                                0.8, (255, 255, 255), 2)
+            break
         cv2.imshow('img', img)
-        if cv2.waitKey(30) == 27 :
+        if cv2.waitKey(30) == 27:
             break
     logging.info('ending')
-
